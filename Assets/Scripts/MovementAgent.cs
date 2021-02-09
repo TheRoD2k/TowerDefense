b418c91c0233628f1825abedd7b86971f08740cc
TheRoD2k <ponomarev.artem@phystech.edu>
2021-02-10 02:25:44 +0300

    First Homework

--- Assets/Scripts/MovementAgent.cs
+++ Assets/Scripts/MovementAgent.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class MovementAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
-    private float m_Speed;
-    [SerializeField]
-    private Vector3 m_Target;
+    private Vector3 m_Speed = new Vector3(0f, 0f, 0f);
 
-    private const float TOLERANCE = 0.1f;
+    [SerializeField] 
+    private Vector3 m_Center = new Vector3(0f, 0f, 0f);
+    [SerializeField]
+    private float m_Center_Mass = 10f;
+    
+    private const float TOLERANCE = 0.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        m_Speed = 5f;
-        m_Target = new Vector3(10f, 0f, 0f);
+        //m_Speed = 0f;
+        //m_Target = new Vector3(10f, 0f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private const float k_Grav_Const = 0.0001f;
+    
+    void FixedUpdate()
     {
-        float distance = (m_Target - transform.position).magnitude;
-        if (distance < TOLERANCE)
+        Application.targetFrameRate = 144;
+
+        float distance = (m_Center - transform.position).magnitude;
+        //if (distance < TOLERANCE)
+        //{
+        //    return;
+       // }
+        Vector3 acceleration_direction = m_Center - transform.position;
+        Vector3 acceleration = k_Grav_Const * m_Center_Mass * acceleration_direction.normalized /
+            acceleration_direction.magnitude / acceleration_direction.magnitude;
+
+        if (distance < 5*TOLERANCE)
         {
-            return;
+            acceleration = acceleration_direction * 0f;
         }
-        Vector3 dir = (m_Target - transform.position).normalized;
-        Vector3 delta = dir * (m_Speed * Time.deltaTime);
+        
+        m_Speed = m_Speed + (acceleration * Time.deltaTime/2);
+        Vector3 delta = m_Speed * Time.deltaTime;    
+
+        
         transform.Translate(delta);
     }
 }
